a0596ce14967f5de0dc96d326395e1915b0843d3
accban2006 <chibao200065@gmail.com>
2025-03-25 15:07:24 +0700

    Add files via upload

---
+++ lab4q2b/src/Shape.java
@@ -0,0 +1,20 @@
+public class Shape(){
+    protected String color;
+    public Shape(String color){
+        boolean filled;
+        this.color = color;
+    }
+    public void setColor(String color){
+        String color = red;
+    }
+    boolean isfilled = true;
+    public void setFilled(){
+        boolean filled;
+    }
+    public void setIsfilled(boolean isfilled) {
+        this.isfilled = isfilled;
+    }
+    public String toString(){
+        return "Shape[color=" + color + "," + "Filled" + isfilled + "]" ;
+    }
+}
